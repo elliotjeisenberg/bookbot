c17a445e123a1ddcca34fdea5125050e022ff675
elliotjeisenberg <elliotjeisenberg@gmail.com>
2025-03-17 01:22:18 -0500

    main and stats

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_book_word_count, get_character_count
+from stats import get_book_word_count, get_character_count, sort_characters_in_dict_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,11 +8,22 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     word_count = get_book_word_count(book_text)
     character_counts = get_character_count(book_text)
-    print(word_count,'words found in the document')
-    print(character_counts)
+    sorted_character_counts = sort_characters_in_dict_list(character_counts)
+    print('== == == == == == BOOKBOT == == == == == ==')
+    print('Analyzing book found at books/frankenstein.txt...')
+    print('----------- Word Count ----------')
+    print('Found',word_count,'total words')
+    print('--------- Character Count -------')
+    for character_count in sorted_character_counts:
+        print(character_count['char'] + ': ' + str(character_count['count']))
 
 
 
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 def get_book_word_count(book_text:str):
     return len(book_text.split())
 
+
 def get_character_count(book_text:str):
     character_counts = {}
     for character in book_text.lower():
@@ -10,3 +11,12 @@ def get_character_count(book_text:str):
             character_counts[character] = 1
 
     return character_counts
+
+
+def sort_characters_in_dict_list(character_counts:dict):
+    sorted_chars = []
+    for key, value in character_counts.items():
+        if key.isalpha():
+            sorted_chars.append({"char":key,"count":value})
+    x = sorted(sorted_chars, key=lambda x: x["count"], reverse=True)
+    return x
